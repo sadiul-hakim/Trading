bd1bdf497828d237a52c4cf8c5596809a67fa89b
Sadiul Hakim <sadiulhakim@gmail.com>
2025-02-12 17:56:52 +0600

    Threading with Virtual Threads

--- src/main/java/xyz/sadiulhakim/config/AppConfig.java
+++ src/main/java/xyz/sadiulhakim/config/AppConfig.java
@@ -4,12 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.TaskScheduler;
-import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 
 @Configuration
 public class AppConfig {
@@ -25,14 +22,9 @@ public class AppConfig {
     }
 
     @Bean
-    TaskScheduler scheduledTaskScheduler() {
-
-        // Define a dynamic thread pool size based on available system resources
-        int availableProcessors = Runtime.getRuntime().availableProcessors();
-        int poolSize = Math.max(availableProcessors, 10); // Ensure at least 10 threads
-
-        ScheduledExecutorService scheduledVirtualExecutor = Executors.newScheduledThreadPool(poolSize, Thread.ofPlatform().factory());
-        return new ConcurrentTaskScheduler(scheduledVirtualExecutor);
+    public TaskScheduler scheduledTaskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setThreadFactory(Thread.ofVirtual().factory());
+        return scheduler;
     }
-
 }
--- src/main/java/xyz/sadiulhakim/socket/SocketConfig.java
+++ src/main/java/xyz/sadiulhakim/socket/SocketConfig.java
@@ -3,23 +3,17 @@ package xyz.sadiulhakim.socket;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 
 @Configuration
 @EnableWebSocketMessageBroker
 @EnableWebSocket
 @EnableScheduling
 public class SocketConfig implements WebSocketMessageBrokerConfigurer {
-
-    private static final int POOL_SIZE = 10; // Choose an appropriate thread pool size
-    private final ScheduledExecutorService scheduledExecutor = Executors.newScheduledThreadPool(POOL_SIZE);
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -30,8 +24,12 @@ public class SocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
 
         // Use a shared scheduled executor
-        ConcurrentTaskScheduler taskScheduler = new ConcurrentTaskScheduler(scheduledExecutor);
-        registry.enableSimpleBroker("/topic", "/queue").setTaskScheduler(taskScheduler);
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setThreadFactory(Thread.ofVirtual().factory());
+        scheduler.initialize();
+
+        registry.enableSimpleBroker("/topic", "/queue")
+                .setTaskScheduler(scheduler);
 
         registry.setApplicationDestinationPrefixes("/stock");
         registry.setUserDestinationPrefix("/user");
